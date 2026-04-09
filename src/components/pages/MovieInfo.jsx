d4172c17d6999466064ad333df8d0fd9faf5261c
jkarnwie2000 <jkarnwie2000@yahoo.com>
2026-04-09 07:20:34 -0400

    .\.git\

--- src/components/pages/MovieInfo.jsx
+++ src/components/pages/MovieInfo.jsx
@@ -1,61 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import axios from 'axios';
-
-
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
 
 const MovieInfo = () => {
-const { id } = useParams();
-const [movie, setMovie] = useState({});
+  const { id } = useParams();
+  const [movie, setMovie] = useState({});
 
-useEffect(() => {
-const fetchMovieById = async () => {
-const res = await axios.get(`https://www.omdbapi.com/?apikey=3608d43&i=${id}`);
-const data = res.data
-setMovie(data);
-}
-fetchMovieById();
-}, [])
+  useEffect(() => {
+    const fetchMovieById = async () => {
+      const res = await axios.get(
+        `https://www.omdbapi.com/?apikey=3608d43&i=${id}`,
+      );
+      const data = res.data;
+      setMovie(data);
+    };
+    fetchMovieById();
+  }, [id]);
 
-return (
-    <div id='movies__body'>
-        <main id='movies__main'>
-            <div className='movies__container'>
-                <div className='row'>
-                    <div className='movie__selected--top'>
-                        <Link to='/movies' className='' >
-                            <FontAwesomeIcon icon='arrow-left' />
-                        </Link>
-                        <Link to='/movies' className=''>
-                        <h2 movie__selected--title--top>Movies</h2>                        
-                        </Link>
-                    </div>
-                <div className='movie__selected'>
-                    <figure className='movie__selected--figure'>
-                        <img src={movie.Poster} alt="" className='movie__selected--img'/>
-                    </figure>
-                    <div className='movie__selected--description'>
-                        <h2 className='movie__selected--title'>
-                            {movie.Title}
-                        </h2>                                             
-                        <div className='movie__selected--price'></div>
-                        <div className='movie__summary'>
-                            <h3 className='movie__summary--title'>
-                                Summary
-                            </h3>
-                            <p className='movie__summary--para'>
-                            {movie.Plot}
-                            </p>                            
-                        </div>                                           
-                    </div>
-                </div>
+  return (
+    <div id="movies__body">
+      <main id="movies__main">
+        <div className="movies__container">
+          <div className="row">
+            <div className="movie__selected--top">
+              <Link to="/movies" className="">
+                <FontAwesomeIcon icon="arrow-left" />
+              </Link>
+              <Link to="/movies" className="">
+                <h2 movie__selected--title--top>Movies</h2>
+              </Link>
             </div>
-        </div>        
-    </main>
-</div>
-  )
-}
+            <div className="movie__selected">
+              <figure className="movie__selected--figure">
+                <img
+                  src={movie.Poster}
+                  alt=""
+                  className="movie__selected--img"
+                />
+              </figure>
+              <div className="movie__selected--description">
+                <h2 className="movie__selected--title">{movie.Title}</h2>
+                <div className="movie__selected--price"></div>
+                <div className="movie__summary">
+                  <h3 className="movie__summary--title">Summary</h3>
+                  <p className="movie__summary--para">{movie.Plot}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
 
-export default MovieInfo
-
+export default MovieInfo;
--- src/components/pages/Movies.jsx
+++ src/components/pages/Movies.jsx
@@ -2,29 +2,41 @@ import { useState } from "react";
 import Movie from "../ui/Movie";
 import axios from "axios";
 
-
-
 const Movies = () => {
-const [movies, setMovies] = useState([]);
-const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
+//   function filterMovies(filter) {
+//     if (filter === "LOW_TO_HIGH") {
+//       setMovies(
+//         movies
+//           .slice()
+//           .sort(
+//             (a, b) =>
+//               (a.salePrice || a.originalPrice) -
+//               (b.salePrice || b.originalPrice),
+//           ),
+//       );
+//     }
 
-function filterMovies(filter) {
+//     if (filter === "HIGH_TO_LOW") {
+//       setMovies(
+//         movies
+//           .slice()
+//           .sort(
+//             (a, b) =>
+//               (b.salePrice || b.originalPrice) -
+//               (a.salePrice || a.originalPrice),
+//           ),
+//       );
+//     }
 
-    if (filter === "LOW_TO_HIGH") {
-        setMovies(movies.slice().sort((a, b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)))
-    }
+//     if (filter === "RATING") {
+//       setMovies(movies.slice().sort((a, b) => b.rating - a.rating));
+//     }
+//   }
 
-    if (filter === "HIGH_TO_LOW") {
-        setMovies(movies.slice().sort((a, b) => (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice)))
-    }
-
-    if (filter === "RATING") {
-        setMovies(movies.slice().sort((a, b) => b.rating - a.rating))
-    }
-}
-
-async function fetchMovies(e) {
+  async function fetchMovies(e) {
     e.preventDefault();
     const res = await axios.get(
       `https://www.omdbapi.com/?apikey=25cd594&s=${searchTerm}`,
@@ -33,36 +45,39 @@ async function fetchMovies(e) {
     setMovies(data);
   }
 
-return (
+  return (
     <div className="movies__body">
-        <div id="main" className="movies__main">
-            <section>
-                <div className="search">
-                <h2 className="section__title">Find Your Movies Here</h2>
-                <form className="search__form" onSubmit={fetchMovies}>
-                    <input type="text" placeholder="Search for movies..." onChange={(e) => setSearchTerm(e.target.value)}/>
-                    <button onClick={fetchMovies}>
-                        Search
-                    </button>
-                </form>
-                </div>
-                <div className="movies__container">
-                    <div className="row">
-                        <div className="movies__header">
-                            <h2 className="section__title movies__header--title">All Movies</h2>
-                            
-                        </div>
-                        <div className="movies">
-                        {
-                            movies.map(movie => <Movie movie={movie} key={movie.id}/>)
-                        }
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
+      <div id="main" className="movies__main">
+        <section>
+          <div className="search">
+            <h2 className="section__title">Find Your Movies Here</h2>
+            <form className="search__form" onSubmit={fetchMovies}>
+              <input
+                type="text"
+                placeholder="Search for movies..."
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+              <button onClick={fetchMovies}>Search</button>
+            </form>
+          </div>
+          <div className="movies__container">
+            <div className="row">
+              <div className="movies__header">
+                <h2 className="section__title movies__header--title">
+                  All Movies
+                </h2>
+              </div>
+              <div className="movies">
+                {movies.map((movie) => (
+                  <Movie movie={movie} key={movie.id} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Movies
+export default Movies;
