5488100bf696be0f3bf515acd810d242ee2d1003
jkarnwie2000 <jkarnwie2000@yahoo.com>
2026-09-22 05:51:05 -0400

    Refine photo page layout

--- src/components/pages/MovieInfo.jsx
+++ src/components/pages/MovieInfo.jsx
@@ -1,58 +1,109 @@
-import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-const MovieInfo = () => {
-  const { id } = useParams();
-  const [movie, setMovie] = useState({});
+const MovieInfo = ({ movies, addToCart, cart }) => {
+const { id } = useParams();
+const [movie, setMovie] = useState(null);
+const [searchTerm, setSearchTerm] = useState("");
 
-  useEffect(() => {
-    const fetchMovieById = async () => {
-      const res = await axios.get(
-        `https://www.omdbapi.com/?apikey=3608d43&i=${id}`,
-      );
-      const data = res.data;
-      setMovie(data);
-    };
-    fetchMovieById();
-  }, [id]);
 
-  return (
-    <div id="movies__body">
-      <main id="movies__main">
+function addMovieToCart(movie) {
+    addToCart(movie);
+}
+
+function movieExistsOnCart() {
+    return cart.find(item => item.id === movie.id);
+}
+useEffect(() => {
+  axios
+    .get(`https://www.omdbapi.com/?apikey=25cd594&s=${searchTerm}`,)
+    .then(response => {
+      setMovie(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}, [id]);
+
+if (!movie) {
+  return <p>Loading...</p>;
+}
+
+console.log("Movie data:", movie);
+
+return (
+<div id="movies__body">
+        <main id="movies__main">
         <div className="movies__container">
-          <div className="row">
-            <div className="movie__selected--top">
-              <Link to="/movies" className="">
-                <FontAwesomeIcon icon="arrow-left" />
-              </Link>
-              <Link to="/movies" className="movie__selected--title--top">
-                <h2 className="movie__selected--title--top">Movies</h2>
-              </Link>
-            </div>
-            <div className="movie__selected">
-              <figure className="movie__selected--figure">
-                <img
-                  src={movie.Poster}
-                  alt=""
-                  className="movie__selected--img"
-                />
-              </figure>
-              <div className="movie__selected--description">
-                <h2 className="header__title">{movie.Title}</h2>
-                <div className="movie__selected--price"></div>
-                <div className="movie__summary">
-                  <h3 className="movie__summary--title">Summary</h3>
-                  <p className="movie__summary--para">{movie.Plot}</p>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="row">
+        <div className="movie__selected--top">
+            <Link to="/movies" className="movie__link">
+            <FontAwesomeIcon icon="arrow-left" />
+            </Link>
+            <Link to="/movies" className="movie__link">
+            <h2 className="movie__selected--title--top">Movies</h2>
+            </Link>  
         </div>
-      </main>
-    </div>
-  );
-};
+        <div className="movie__selected">
+            <figure className="movie__selected--figure">
+              <img
+                src={movie.Poster}
+                alt={movie.Title}
+                className="movie__selected--img"
+              />
+            </figure>
+            <div className="movie__selected--description">
+                <h2 className="movie__selected--title">
+                    {movie.Title}
+                </h2>
+        
+        <div className="movie__summary">
+            <h3 className="movie__summary--title">
+                Summary
+            </h3>
+            <p className="movie__summary--para">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                Culpa in amet optio, molestiae, laboriosam esse atque 
+                consequatur perspiciatis ut quo, quam praesentium consequuntur 
+                harum necessitatibus quaerat nulla beatae neque provident!
+            </p>
+            <p className="movie__summary--para">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                Culpa in amet optio, molestiae, laboriosam esse atque 
+                consequatur perspiciatis ut quo, quam praesentium consequuntur 
+                harum necessitatibus quaerat nulla beatae neque provident!
+            </p>
+        </div>
+        {movieExistsOnCart() ? 
+        (
+        <Link to={`/cart`} className="movie__link">  
+        <button className="btn">Checkout</button>
+        </Link>
+        ) 
+        : (
+        <button className="btn" onClick={() => addMovieToCart(movie)}>Add to cart</button>   
+            )}
+        </div>
+        </div>
+        </div>
+        </div>
 
-export default MovieInfo;
+            <div className="movies__container">
+                <div className="row">
+                    <div className="movie__selected--top">
+                    <h2 className="movie__selected--title--top">
+                        Recommended Movies
+                    </h2>
+                    </div>                    
+            </div>
+            </div>
+        </main>        
+  </div>   
+  
+)
+} 
+export default MovieInfo
+
